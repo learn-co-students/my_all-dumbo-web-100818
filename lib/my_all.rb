5ccea62fba3e12d84b67892d125d400c2354bbab
said shah <saidmysr20@aol.com>
2018-10-09 18:43:38 -0400

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+counter = 0
+return_values=[]
+while counter < collection.length
+return_values << yield(collection[counter])
+counter += 1
+  end
+  if return_values.include?(false)
+      false
+  else
+      true 
+  end
+
 
 end
